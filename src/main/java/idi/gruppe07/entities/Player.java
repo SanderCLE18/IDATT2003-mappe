983005bf6200516a42c14efe2c83588da3d0de4d
fletcher99 <herman.falchener@gmail.com>
2026-02-12 12:59:27 +0100

    Created and implemented the Player class

---
+++ src/main/java/idi/gruppe07/entities/Player.java
@@ -0,0 +1,44 @@
+package idi.gruppe07.entities;
+
+import java.math.BigDecimal;
+
+public class Player {
+  private String name;
+  private BigDecimal startingMoney;
+  private BigDecimal money;
+  private Portfolio portfolio;
+
+  // private TransactionArchive transactionArchive;
+  public Player(String name, BigDecimal startingMoney) {
+    this.name = name;
+    this.startingMoney = startingMoney;
+    this.money = startingMoney;
+    this.portfolio = new Portfolio();
+    // this.transactionArchive = new Transaction Archive;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public BigDecimal getMoney() {
+    return money;
+  }
+
+  public void addMoney(BigDecimal money) {
+    this.money = this.money.add(money);
+  }
+
+  public void withdrawMoney(BigDecimal money) {
+    this.money = this.money.subtract(money);
+  }
+
+  public Portfolio getPortfolio() {
+    return portfolio;
+  }
+
+  /* public TransactionArchive getTransactionArchive () {
+    return transactionArchive;
+  }
+  */
+}
