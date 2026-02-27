3080ca309875ff74572a3e4c7cc673f81208c8fd
fletcher99 <herman.falchener@gmail.com>
2026-02-27 13:30:36 +0100

    #17 close Created unit-tests for apropriate methods in all the classes in entities

---
+++ src/test/java/idi/gruppe07/entities/PortfolioTest.java
@@ -0,0 +1,44 @@
+package idi.gruppe07.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PortfolioTest {
+
+
+  Portfolio portfolio;
+  Stock stock;
+  Share share;
+
+  @BeforeEach
+  void setUp() {
+    portfolio = new Portfolio();
+    List<BigDecimal> salesPrice = new ArrayList<>();
+    salesPrice.add(new BigDecimal("150.00"));
+    stock = new Stock("AAPL", "Apple Inc.", salesPrice);
+    share = new Share(stock, BigDecimal.valueOf(10), BigDecimal.valueOf(150.00));
+  }
+
+  @Test
+  void checkThatShareIsAddedToShares() {
+    assertTrue(portfolio.addShare(share));
+  }
+
+  @Test
+  void checkThatShareIsSuccesfullyRemoved() {
+    portfolio.addShare(share);
+    assertTrue(portfolio.removeShare(share));
+  }
+
+  @Test
+  void checkThatObjectContainsAddedShare() {
+    portfolio.addShare(share);
+    assertTrue(portfolio.contains(share));
+  }
+}
