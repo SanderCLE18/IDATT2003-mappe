1e34e6b5f8f86fbf874a2d50db90a3c24c5060cf
fletcher99 <herman.falchener@gmail.com>
2026-02-27 12:16:33 +0100

    added idea to gitignore and created test-folder for unit-tests

---
+++ src/test/java/idi/gruppe07/entities/PlayerTest.java
@@ -0,0 +1,12 @@
+package idi.gruppe07.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+  @Test
+  void getPortfolio() {
+  }
+}
