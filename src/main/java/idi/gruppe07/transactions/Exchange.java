dc7d726fa69610ce4d4099c8847f91a5ccbb4fac
sandercl <sandercl@stud.ntnu.no>
2026-02-27 12:30:05 +0100

    Added documentation for Exchange and NormalDistribution classes. Fixed abstract Transaction class.

--- src/main/java/idi/gruppe07/transactions/Exchange.java
+++ src/main/java/idi/gruppe07/transactions/Exchange.java
@@ -11,42 +11,80 @@ import java.util.Map;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+/**
+ * Represents a stock exchange where players can trade stocks.
+ */
 public class Exchange {
   private String name;
   private int week;
   private Map<String, Stock> stockMap;
   private Random random;
 
-  public Exchange(String name, List<Stock> stocks) {
+  /**
+   * Constructs an exchange with the given name and stocks.
+   * @param name The name of the exchange.
+   * @param stocks The list of stocks in the exchange.
+   *
+   * @throws NullPointerException if stocks is null.
+   * @throws IllegalArgumentException if stocks is empty.*/
+
+  public Exchange(String name, List<Stock> stocks) throws NullPointerException, IllegalArgumentException{
     this.name = name;
     stockMap = stocks.stream()
         .collect(Collectors.toMap(
             Stock::getSymbol, stock -> stock));
   }
-
+  /**
+   * Gets the name of the exchange.
+   * @return name The name of the exchange.
+   * */
   public String getName() {
     return name;
   }
-
+  /**
+   * Gets the current week of the exchange.
+   * @return week The current week of the exchange.
+   * */
   public int getWeek() {
     return week;
   }
-
+  /**
+   * Checks if the exchange has a stock with the given symbol.
+   * @param symbol The symbol of the stock to check.
+   * @return true if the exchange has the stock, false otherwise.*/
   public boolean hasStock(String symbol) {
     return stockMap.containsKey(symbol);
   }
 
+  /**
+   * Gets the stock with the given symbol.
+   * @param symbol The symbol of the stock to get.
+   * @return stock The stock with the given symbol.*/
   public Stock getStock(String symbol) {
     return stockMap.get(symbol);
   }
-
+  /**
+   * Gets a list of stocks that contain the given search string in their company name.
+   * @param search The string to search for in the company name.
+   * @return stocks A list of stocks that contain the search string.
+   * @throws NullPointerException if search is null.
+   * @throws IllegalArgumentException if search is empty.
+   * */
   public List<Stock> findStocks(String search){
     return stockMap.values().stream()
         .filter(n -> n.getCompany().toUpperCase().contains(search.toUpperCase()))
         .collect(Collectors.toList());
   }
 
-  //INGEN AV DE UNDER LEGGER TIL I TRANSACTION ARCHIVE!
+  /**
+   * Buys a share of a stock with the given symbol and quantity from the exchange.
+   * @param symbol The symbol of the stock to buy.
+   * @param quantity The quantity of shares to buy.
+   * @param player The player who is buying the stock.
+   * @return transaction The transaction object representing the buy transaction.
+   * @throws NullPointerException if symbol or player is null.
+   * @throws IllegalArgumentException if symbol is empty or player is not a valid player.
+   * @throws IllegalStateException if the exchange does not have the stock.*/
   public Transaction buy(String symbol, BigDecimal quantity, Player player){
     if(hasStock(symbol)){
       Share newShare = new Share(getStock(symbol), quantity, getStock(symbol).getPrice());
@@ -55,11 +93,23 @@ public class Exchange {
     }
     else return null;
   }
+  /**
+   * Sells a share of a stock from the player's portfolio.
+   * @param share The share to sell.
+   * @param player The player who is selling the stock.
+   * @return transaction The transaction object representing the sell transaction.
+   * @throws NullPointerException if share or player is null.
+   * @throws IllegalArgumentException if share is not a valid share or player is not a valid player.
+   * @throws IllegalStateException if the player does not own the share.*/
   public Transaction sell(Share share, Player player){
     player.getPortfolio().removeShare(share);
     player.addMoney(share.getQuantity().multiply(share.getStock().getPrice()));
     return new Sale(share, getWeek());
   }
+
+  /**
+   * Advances the exchange by one week.
+   * Sets new prices for each stock based on a normal distribution.*/
   public void advance(){
     this.week++;
     NormalDistribution distribution = new NormalDistribution(0, 0.05);
--- src/main/java/idi/gruppe07/utils/NormalDistribution.java
+++ src/main/java/idi/gruppe07/utils/NormalDistribution.java
@@ -1,18 +1,28 @@
 package idi.gruppe07.utils;
 
 import java.util.Random;
-
+/**
+ * Represents a normal distribution.
+ * */
 public class NormalDistribution {
   Random r = new Random();
   //Mu
   double mean;
   //Sigma
   double stdDev;
-
+  /**
+   * Constructs a normal distribution with the given mean and standard deviation.
+   * @param mean The mean of the distribution.
+   * @param stdDev The standard deviation of the distribution.
+   * @throws IllegalArgumentException if stdDev is less than or equal to 0.
+   * @throws NullPointerException if mean is null.*/
   public NormalDistribution(double mean, double stdDev) {
     this.mean = mean;
     this.stdDev = stdDev;
   }
+  /**
+   * Generates a random number from the normal distribution.
+   * @return A random number from the normal distribution.*/
   public double nextGaussian() {
     return r.nextGaussian() * stdDev + mean;
   }
